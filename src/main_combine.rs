f13f54c7a1cc134b63b304b966cc94e3579af114
idursun <ibrahim.dursun@gmail.com>
2019-03-12 23:51:42 +0000

    Add combine parser sample

---
+++ src/main_combine.rs
@@ -0,0 +1,89 @@
+use combine::char::space;
+use combine::parser::range::{range, take_while1};
+use combine::stream::state::State;
+use combine::{
+    attempt, between, choice, optional, sep_by1, skip_many, token, ParseError, Parser, RangeStream,
+};
+
+#[derive(Debug, PartialEq)]
+struct Statement<'a> {
+    columns: Vec<Object<'a>>,
+    table: Object<'a>,
+}
+
+#[derive(Debug, PartialEq)]
+struct Object<'a> {
+    schema: Option<&'a str>,
+    name: &'a str,
+}
+
+fn ident<'a, I>() -> impl Parser<Input = I, Output = &'a str>
+where
+    I: RangeStream<Item = char, Range = &'a str>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    take_while1(|c: char| c.is_alphabetic())
+}
+
+fn delimited_ident<'a, I>() -> impl Parser<Input = I, Output = &'a str>
+where
+    I: RangeStream<Item = char, Range = &'a str>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    between(token('['), token(']'), ident())
+}
+
+fn p_object_name<'a, I>() -> impl Parser<Input = I, Output = Object<'a>>
+where
+    I: RangeStream<Item = char, Range = &'a str>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    (
+        skip_many(space()),
+        optional(delimited_ident().skip(token('.'))),
+        choice((attempt(delimited_ident()), ident())),
+        skip_many(space()),
+    )
+        .map(|(_, schema, name, _)| Object { schema, name })
+}
+
+fn column_list<'a, I>() -> impl Parser<Input = I, Output = Vec<Object<'a>>>
+where
+    I: RangeStream<Item = char, Range = &'a str>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    sep_by1(p_object_name(), range(","))
+}
+
+fn p_select<'a, I>() -> impl Parser<Input = I, Output = Statement<'a>>
+where
+    I: RangeStream<Item = char, Range = &'a str>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    (
+        range("select"),
+        skip_many(space()),
+        column_list(),
+        skip_many(space()),
+        range("from"),
+        p_object_name(),
+    )
+        .map(|(_, _, columns, _, _, table)| Statement { columns, table })
+}
+
+fn main() {
+    match p_select().easy_parse(State::new("select a,b,c,d from table1")) {
+        Ok((output, _remaining)) => println!("{:?}", output),
+        Err(err) => eprintln!("{}", err),
+    };
+
+    match column_list().easy_parse(State::new("[a].[b] ,  [x].[y]")) {
+        Ok((output, _remaining)) => println!("{:?}", output),
+        Err(err) => eprintln!("{}", err),
+    };
+
+    match p_object_name().easy_parse(State::new("  [a].[b]  ")) {
+        Ok((output, _remaining)) => println!("{:?}", output),
+        Err(err) => eprintln!("{}", err),
+    };
+}
